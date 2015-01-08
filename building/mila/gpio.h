f26a9a166e0aa04c482c32ba6d6ea8d31ebb772d
dubkov <dubkov@skbrii.ru>
2015-01-09 00:41:19 +0600

    gotcha

---
+++ building/mila/gpio.h
@@ -0,0 +1,56 @@
+#ifndef gpio_h
+#define gpio_h
+
+///### PORTS SECTION ###\\\
+/*
+#define	A0	0
+#define	A1	1
+#define	A2	2
+#define	A3	3
+#define	A4	4
+#define	A5	5
+#define	A6	6
+#define	A7	7
+
+#define	B0	0
+#define	B1	1
+#define	B2	2
+#define	B3	3
+#define	B4	4
+#define	B5	5
+#define	B6	6
+#define	B7	7
+#define	B8	8
+#define	B9	9
+#define	B10	10
+
+#define	C0	0
+#define	C1	1
+#define	C2	2
+
+#define	D0	0
+#define	D1	1
+#define	D2	2
+#define	D3	3
+#define	D4	4
+#define	D5	5
+#define	D6	6
+#define	D7	7
+
+#define	C0	0
+#define	C1	1
+#define	C2	2
+#define	C3	3
+#define	C6	6
+#define	C7	7
+
+#define	F0	0
+#define	F1	1
+#define	F2	2
+#define	F3	3
+#define	F4	4
+#define	F5	5
+#define	F6	6
+#define	F7	7
+*/
+#endif
