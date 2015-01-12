53735297c2bf0524b7682d11c97fcc5e07e90d8c
dubkov <dubkov@skbrii.ru>
2015-01-12 11:51:20 +0700

    deleted:    Makefile~ 	modified:   README.md 	modified:   burner.sh 	renamed:    1986_BOOT_UART.HEX -> firmwarrior/1986_BOOT_UART.HEX 	new file:   firmwarrior/fw.cfg 	new file:   firmwarrior/fw.py 	renamed:    blink.c -> legacy/blink.c 	renamed:    second.c -> legacy/second.c

--- burner.sh
+++ burner.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-python3 /home/nes/devel/mila-fw/fw.py -P -p /dev/ttyUSB0 -e -f /home/nes/devel/mila/building/test.hex -V -r 
+python3 firmwarrior/fw.py -P -p /dev/ttyUSB0 -e -f test.hex -V -r 
