288498db953dcb7d95d7fba9a75b71ef7f8490f1
Юрий <ko10ok@users.noreply.github.com>
2015-01-18 17:16:53 +0600

    Move suggestion to Issue

--- src/mila/gpio.c
+++ src/mila/gpio.c
@@ -99,22 +99,6 @@ int pinRead(int pin){
 
 // must check if args quantity match corresponding port pins quantity
 
-//FEATURE: mask modificator usage concept:
-// "0х04 OE[15:0] MDR_PORTx->OE" means 16bit of "uint32_t mode" used?
-//...
-//#define IN 0x0000?
-//#define OUT 0xFFFF?
-//#define OUT_P(pin) ~((1) << (pin))
-//#define IN_P(pin) ((1) << (pin)) 
-//
-//if(mode&&0x0000FFFF == OUT)..
-//	port->OE |= 0xFFFFFFFFF && (mode)
-//   elif(mode&&0x0000FFFF == IN)
-//	port->OE |= 0x000000000 | (mode)
-//
-//exmpl usage: portInit(PB,IN | OUT_P(2) | OUT_P(3) ) //kinda windows-like style of parameter compilation
-//	       portInit(PB,OUT | IN_P(2) | IN_P(6) | IN_P(7) ) //kinda windows-like style of parameter compilation
-
 void portInit(MDR_PORT_TypeDef *port, uint32_t mode){
   if (mode == OUT){
     port->OE |= 0xFFFFFFFF;
@@ -164,77 +148,3 @@ void snake(){
   pinWrite(11,0);
   delay();
 }
-
-
-//SECTION to check, recode and approve
-
-//FEATURE: write to port in 'write(PB,PB3,PB4,PC..,(long)0X012312341FDH)'- like format
-/*
-ports = 43 bits;
-4 highest should be 0x0000 .... .... .... .....
-size of long = 64 bit
-
-type(porttypesize p)
-{
-  //например префикс и постфикс 01 - порт, ff - пин
-  if(0xFFFF & p == 0x0000)
-    return чиселко
-  else: if(0xF000 & p == 0x0000)
-          return пин
-        else
-          return порт
-}
-
-вот сюда выше идея:
-0xWP0p
-
-W - whole port
-P - port
-p - pin
-
-целые порты типа PA, PB дефайнить как 0x1000 0x2000
-пин на порту типа B2 B3 дефайнить как 0x0202 0x0203
-сквозная нумерация пинов делается с пустым значением WP например 0х000А 0х00В2
-тогда всё это сможет кушать pinWrite.
-
-void write(porttypesize port, ...) {
-    MDR_PORT_TypeDef *port;
-    porttypesize ports[100];
-    index = 0;
-
-    porttypesize *p = &first;
-
-    while (type(p) != чиселко) {
-      ports[index++]=p;
-      p++;
-    }
-
-    long value = p;
-    bitOffset=0;
-    //запись значения 
-    for(i=0;i<count;i++)
-      if(type(ports[i])==порт):
-      {
-          port = port_from_pin( (ports[i] >> 2) & 0x0F);
-          port->RXTX = (value << bitOffset) 0xFFFFFFFF;
-          bitOffset+=_sizeofbytesinint_;
-
-          break;
-      }
-      else:
-      {
-          //для скорости не делаем пинврайт
-          if (value == 1){
-            port->RXTX |= ( 1 << bit );
-          }
-
-          else if (val == 0){
-            port->RXTX &= ~( 1 << bit );
-          }
-          bitOffset+=_sizeofbytesforbit_;
-
-          break;
-      }
-}
- 
-*/
