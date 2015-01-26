251d684bcbd4e28522014f971689addb11209287
Илья Дубков <dubkov@users.noreply.github.com>
2015-01-26 08:41:31 +0600

    добавил больше проверок, прокомментировал
    В таком стиле будет писать пользователь. Данный код служит для проверки, что внутренности наших библиотек работают.

--- linux/usercode.c
+++ linux/usercode.c
@@ -11,26 +11,31 @@
 	// BTN RIGHT PB6
 
 void init(){
-	portInit(PORTB,OUT);
-	pinInit(B5,IN);
-	pinInit(B6,IN);
+	portInit(PORTB,OUT); 	//проверка инициализации порта
+	pinInit(B5,IN);		//проверка инициализации пинов поверх инициализированного порта
+	pinInit(B6,IN);		
 
-	portWrite(PORTB,0xF);
+	portWrite(PORTB,0xF);	//проверка записи в порт
 	delay();
 	int i=8;
 	for(i=8;i<12;i++){
-		pinWrite(i,0);
+		pinWrite(i,0);	//проверка записи в пины по виртуальным номерам
 		delay();
 	}
-	for(i=8;i<12;i++){
-		pinWrite(i,1);
-		delay();
-	}
+	pinWrite(B0,1); 	//проверка записи в пины по названию "порт-пин"
+	delay();
+	pinWrite(B1,HIGH); 	
+	delay();
+	pinWrite(B2,1); 	
+	delay();
+	pinWrite(B3,1); 	
+	delay();
+	
 	portWrite(PORTB,0);
 	delay();
 }
 
 void cycle(){
-	pinWrite(B0,1-pinRead(B5));
-	pinWrite(B3,1-pinRead(B6));
+	pinWrite(8,1-pinRead(13));	//проверка чтения пина по виртуальному номеру
+	pinWrite(B3,1-pinRead(B6));	//проверка чтения пина по названию "порт-пин"
 }
